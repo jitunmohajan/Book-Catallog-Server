7567d698a59681982d1f622ebf86f25053c65240
jitunmohajan <jitunmohajan@gmail.com>
2023-09-02 18:19:40 +0600

    get single user Complete

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -4,6 +4,6 @@ import { UserController } from './user.controller';
 const router = express.Router();
 
 router.get('/', UserController.getAllFromDB)
-// router.get('/:id', UserController.getByIdFromDB)
+router.get('/:id', UserController.getByIdFromDB)
 
 export const userRoutes = router;
