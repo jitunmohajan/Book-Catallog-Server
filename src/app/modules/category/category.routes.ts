ecde745e51dce470911ec86601f054a51d1a3581
jitunmohajan <jitunmohajan@gmail.com>
2023-09-02 19:16:43 +0600

    get all Category Complete

--- src/app/modules/category/category.routes.ts
+++ src/app/modules/category/category.routes.ts
@@ -6,7 +6,7 @@ import { CategoryController } from './category.controller';
 const router = express.Router();
 
 router.post('/create-category', validateRequest(CategoryValidation.create),CategoryController.insertIntoDB)
-// router.get('/', CategoryController.getAllFromDB)
+router.get('/', CategoryController.getAllFromDB)
 // router.get('/:id', CategoryController.getByIdFromDB)
 // router.patch('/:id', validateRequest(CategoryValidation.update),CategoryController.updateOneInDB)
 // router.delete('/:id', CategoryController.deleteByIdFromDB)
